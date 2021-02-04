d90fcb03d1b1782b249fd4cab1ca51874ad59ae3
Maria Manriquez <manriquez22446688@gmail.com>
2021-02-04 12:21:22 -0400

    arreglado

--- ui.js
+++ ui.js
@@ -1,39 +1,35 @@
-import {Products, ProductsManagement} from "./products.js";
+import {Products, ProductsManagement} from "./products.js"
 class Ui {
-    constructor() {
+    constructor(){
         this.uiName = document.getElementById("name");
         this.uiDescription = document.getElementById("description");
         this.uiQuantity = document.getElementById("quantity");
-        this.uiForm = document.getElementById("form-data");
-        this.container = document.getElementById("container-table");
-        this.manager =  new ProductsManagement();
+        this.uiForm = document.getElementById ("form-data");
+        this.container = document.getElementById ("container-table");
+        this.manager = new ProductsManagement();
         let p1 = new Products("Pollo", "Pollo Sofia", 20);
-        let p2 = new Products("Galletas", "Galleras Mabel", 50);
+        let p2 = new Products("Galletas", "Galletas Mabel", 50);
         let p3 = new Products("Pollo", "Pollo Imba", 50);
         this.manager.addProducts(p1);
         this.manager.addProducts(p2);
         this.manager.addProducts(p3);
         this.loadEvents();
+        
     }
-    loadEvents() {
-        this.uiForm.addEventListener("submit", (e) => {
+    loadEvents(){
+        this.uiForm.addEventListener("submit", (e)=>{
             e.preventDefault();
             this.addProducts(
                 this.uiName.value,
                 this.uiDescription.value,
-                this.uiQuantity.value);
-            this.clearForm();
+                this.uiQuantity.value
+            );
         });
     }
-    clearForm() {
-                this.uiName.value = "";
-                this.uiDescription.value = ""
-                this.uiQuantity.value = ""
-    }
-    loadTable() {
-        var html = "";
-        for (var i = 0; i < this.manager.showProducts().length; i++) {
-            html += `
+    loadTable(){
+        var html ="";
+        for(var i=0; i< this.manager.showProducts().length; i++){
+            html+= `
             <tr>
                 <td scope="row">
                 ${this.manager.showProducts()[i].name}</td>
@@ -43,7 +39,7 @@ class Ui {
         }
         this.container.innerHTML = html;
     }
-    addProducts(name, description, quantity) {
+    addProducts(name, description, quantity){
         let p1 = new Products(name, description, quantity);
         this.manager.addProducts(p1);
         this.loadTable();
